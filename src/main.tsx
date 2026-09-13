d4133df0edc03e5a2b4b6b4582f3f00b5f59f939
Omer YIGITLER <138784969+omeryigitler@users.noreply.github.com>
2026-09-13 06:11:45 +0200

    style: load final calculator v3 refinements

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+import './calculator-v3-tweaks.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
