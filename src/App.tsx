0dc4dece0ac894ee23bf117fabb4ba73fdae4f59
Omer YIGITLER <138784969+omeryigitler@users.noreply.github.com>
2026-09-14 02:20:10 +0200

    feat: place accounting story between hero and services

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import React, { useEffect } from 'react';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import { Header } from './components/Header';
 import { Hero } from './components/Hero';
+import { AccountingStory } from './components/AccountingStory';
 import { Services } from './components/Services';
 import { KineticMarquee } from './components/KineticMarquee';
 import { Simulator } from './components/Simulator';
@@ -52,6 +53,7 @@ export default function App() {
         <Header />
         <main id="site-content">
           <Hero />
+          <AccountingStory />
           <Services />
           <KineticMarquee />
           <Simulator />
